a162a40e287d59b354f92995aeba9f55557f4104
DarkLord-13 <nishantrajendrakumar@gmail.com>
2025-04-25 21:17:24 +0530

    added feature

---
+++ a.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (i == j) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        scanner.close();
+        System.out.println(dp[n][m]);
+    }
+}
